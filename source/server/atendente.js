90f100c5047aa9aa6b6da742f063f69f94755584
Matheus Batistela <matheusbatistela@alunos.utfpr.edu.br>
2018-11-11 15:27:47 -0200

    js: atendente

---
+++ source/server/atendente.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "agropec.admin",
+  password: "admin",
+  database: "Agropec"
+});
+
+con.connect(function(err) {
+  if (err) throw err;  
+  console.log("Connected!");
+  
+  var sql = "INSERT INTO Atendente (CPF_Atendente,Nro_Vendas) VALUES (8494847494,9)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted", result);
+  });
+
+  var sql = "UPDATE Atendente SET Nro_Vendas = 10 WHERE CPF_Antendente = 8494847494";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated", result);
+  });
+
+//   var sql = "DELETE FROM Atendente WHERE CPF_Atendente = 8494847494";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Number of records deleted: " + result.affectedRows);
+//   });
+
+});
