13cfe58ef594f93c87aa1f7ad088038532a36eed
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2018-11-11 15:51:07 -0200

    Server: Atualizando o diretório de telefone e adicionando CRUD telefone_cliente.

--- source/server/telefone.js
+++ source/server/telefone.js
@@ -17,11 +17,17 @@ con.connect(function(err) {
     console.log("1 record inserted", result);
   });
 
-  var sql = "UPDATE Telefones SET Numero = 4417981844399 WHERE Numero =17981844399 ";
+  var sql = "INSERT INTO Telefones (Numero) VALUES (17991524608)";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log(result.affectedRows + " record(s) updated", result);
+    console.log("1 record inserted", result);
   });
+
+  // var sql = "UPDATE Telefones SET Numero = '4417981844399' WHERE Numero = '17981844399' ";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result.affectedRows + " record(s) updated", result);
+  // });
 
   // var sql = "DELETE FROM Telefones WHERE Numero = 4417981844399 ";
   // con.query(sql, function (err, result) {
