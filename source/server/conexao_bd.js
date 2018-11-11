a870aada7f3952afb7a9ef8558aaade05e93188c
Matheus Batistela <matheusbatistela@alunos.utfpr.edu.br>
2018-11-11 14:04:12 -0200

    js: conexao_bd

--- source/server/conexao_bd.js
+++ source/server/conexao_bd.js
@@ -3,27 +3,14 @@
 *
 */
 var mysql = require('mysql');
-var host = "localhost";
-var user_db = "";
-var password_db = "";
 
 var con = mysql.createConnection({
-    host: host,
-    user: user_db,
-    password: password_db
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Atencao! Conexao com banco ativa.");
-  });
+  host: "localhost",
+  user: "agropec.admin",
+  password: "admin"
+});
 
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Result: " + result);
-    });
-  });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
