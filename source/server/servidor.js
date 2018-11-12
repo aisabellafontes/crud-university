f0ba8df42af64a011163e9669e46d828d3041fbb
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2018-11-11 22:33:04 -0200

    Adicionando e atulizando os arquivos .ejs e servidor.js

--- source/server/servidor.js
+++ source/server/servidor.js
@@ -171,6 +171,420 @@ cliente_edit.delete(function(req,res,next){
      });
 });
 
+var fornecedor = router.route('/fornecedor');
+/* LIST FORNECEDOR */
+fornecedor.get(function(req,res,next){
+
+
+    req.getConnection(function(err,conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query('SELECT * FROM Fornecedor', function(err,rows){
+
+            if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+            }
+
+            res.render('fornecedor',{title:"RESTful Crud Example",data:rows});
+
+         });
+
+    });
+
+});
+
+/* ADD FORNECEDOR */
+fornecedor.post(function(req, res, next){
+
+    //validation  
+    req.assert('id','ID is required').notEmpty();  
+    req.assert('nome','A valid Nome is required').notEmpty();
+    req.assert('tel','A valid Telefone is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {
+        id:req.body.id,
+        nome:req.body.nome,
+        tel:req.body.tel
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("INSERT INTO Fornecedor set ? ", data, function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+var fornecedor_edit = router.route('/fornecedor/:ID');
+
+/* EDIT FORNECEDOR */
+fornecedor_edit.put(function(req,res,next){
+    var fornecedor_id = req.params.id;
+
+    //validation
+    req.assert('id','ID is required').notEmpty();
+    req.assert('nome','A valid name is required').notEmpty();
+    req.assert('tel','A valid Telefone is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {
+        id:req.body.id,
+        nome:req.body.nome,
+        tel:req.body.tel
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("UPDATE Fornecedor set ? WHERE ID = ? ",[data,id], function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+/* DELETE FORNECEDOR */
+fornecedor_edit.delete(function(req,res,next){
+
+    var id = req.params.id;
+
+     req.getConnection(function (err, conn) {
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("DELETE FROM Fornecedor  WHERE id = ? ",[id], function(err, rows){
+
+             if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+             }
+
+             res.sendStatus(200);
+
+        });
+        //console.log(query.sql);
+
+     });
+});
+
+
+var venda = router.route('/venda');
+/* LIST VENDA */
+venda.get(function(req,res,next){
+    
+    
+    req.getConnection(function(err,conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query('SELECT * FROM Vendas', function(err,rows){
+
+            if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+            }
+
+            res.render('venda',{title:"RESTful Crud Example",data:rows});
+
+         });
+
+    });
+
+});
+
+/* ADD VENDA */
+venda.post(function(req, res, next){
+
+    //validation  
+    req.assert('Codigo','Codigo is required').notEmpty();  
+    req.assert('CPF_cliente','A valid CPF_cliente is required').notEmpty();
+    req.assert('CPF_atendente','A valid CPF_atendente is required').notEmpty();
+    req.assert('Tel_cliente','A valid Tel_cliente is required').notEmpty();
+    req.assert('CPF_freteiro','A valid CPF_freteiro is required').notEmpty();
+    req.assert('Data_venda','A valid Data_venda is required').notEmpty();
+    
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {
+        Codigo:req.body.Codigo,
+        CPF_cliente:req.body.CPF_cliente,
+        CPF_atendente:req.body.CPF_atendente,
+        Tel_cliente:req.body.Tel_cliente,
+        CPF_freteiro:req.body.CPF_freteiro,
+        Data_venda:req.body.Data_venda
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("INSERT INTO Vendas set ? ", data, function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+var venda_edit = router.route('/venda/:Codigo');
+
+/* EDIT VENDA */
+venda_edit.put(function(req,res,next){
+    var venda_codigo = req.params.codigo;
+
+    //validation
+    req.assert('Codigo','Codigo is required').notEmpty();  
+    req.assert('CPF_cliente','A valid CPF_cliente is required').notEmpty();
+    req.assert('CPF_atendente','A valid CPF_atendente is required').notEmpty();
+    req.assert('Tel_cliente','A valid Tel_cliente is required').notEmpty();
+    req.assert('CPF_freteiro','A valid CPF_freteiro is required').notEmpty();
+    req.assert('Data_venda','A valid Data_venda is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {
+        Codigo:req.body.Codigo,
+        CPF_cliente:req.body.CPF_cliente,
+        CPF_atendente:req.body.CPF_atendente,
+        Tel_cliente:req.body.Tel_cliente,
+        CPF_freteiro:req.body.CPF_freteiro,
+        Data_venda:req.body.Data_venda
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("UPDATE Vendas set ? WHERE Codigo = ? ",[data,Codigo], function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+/* DELETE VENDA */
+venda_edit.delete(function(req,res,next){
+
+    var Codigo = req.params.Codigo;
+
+     req.getConnection(function (err, conn) {
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("DELETE FROM Vendas  WHERE Codigo = ? ",[Codigo], function(err, rows){
+
+             if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+             }
+
+             res.sendStatus(200);
+
+        });
+        //console.log(query.sql);
+
+     });
+});
+
+
+var categoria = router.route('/categoria');
+/* LIST CATEGORIA */
+categoria.get(function(req,res,next){
+
+
+    req.getConnection(function(err,conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query('SELECT * FROM Categoria', function(err,rows){
+
+            if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+            }
+
+            res.render('categoria',{title:"RESTful Crud Example",data:rows});
+
+         });
+
+    });
+
+});
+
+/* ADD CATEGORIA */
+categoria.post(function(req, res, next){
+
+    //validation    
+    req.assert('ID','ID is required').notEmpty();
+    req.assert('Nome','A Nome id is required').notEmpty();
+    req.assert('Descricao','A valid Descricao is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {        
+        ID:req.body.ID,
+        Nome:req.body.Nome,
+        Descricao:req.body.Descricao
+
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("INSERT INTO Categoria set ? ", data, function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+var categoria_edit = router.route('/cliente/:id');
+
+/* EDIT CATEGORIA */
+categoria_edit.put(function(req,res,next){
+    var user_id = req.params.ID;
+
+    //validation
+    req.assert('ID','ID is required').notEmpty();
+    req.assert('Nome','A Nome id is required').notEmpty();
+    req.assert('Descricao','A valid Descricao is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    //get data
+    var data = {
+        ID:req.body.ID,
+        Nome:req.body.Nome,
+        Descricao:req.body.Descricao
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("UPDATE Categoria set ? WHERE id = ? ",[data,id], function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+
+});
+
+/* DELETE CATEGORIA */
+categoria_edit.delete(function(req,res,next){
+
+    var ID = req.params.ID;
+
+     req.getConnection(function (err, conn) {
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("DELETE FROM Categoria  WHERE id = ? ",[ID], function(err, rows){
+
+             if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+             }
+
+             res.sendStatus(200);
+
+        });
+        //console.log(query.sql);
+
+     });
+});
+
 //now we need to apply our router here
 app.use('/api', router);
 
